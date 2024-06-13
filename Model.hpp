4ba306fbd4a769a3612294ee22bd1a8131d24ac0
andy-azucar <andy.azucar@luminator.com>
2024-06-13 10:20:39 -0500

    Update Model.hpp

--- Model.hpp
+++ Model.hpp
@@ -1,5 +1,9 @@
 #ifndef MODEL_HPP
 #define MODEL_HPP
+#include <touchgfx/widgets/Button.hpp>
+
+extern touchgfx::Button FadeToggleB;
+
 
 class ModelListener;
 
@@ -18,8 +22,14 @@ public:
     void GreenSlider(int value);
     void Blue(int value);
     void White(int value);
+    void FadeF();       // Function prototype for the fade function
+    void toggleFade();
+    void setFadeActive(bool active); // Add this function
+    bool isFadeActive() const; // Add this function
+
 protected:
     ModelListener* modelListener;
+    bool fadeActive = false; // State variable to track fade status
 };
 
 #endif // MODEL_HPP
