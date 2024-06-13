507966543ddd33885fa992e70dd22f05f4ce3a71
andy-azucar <andy.azucar@luminator.com>
2024-06-13 10:21:47 -0500

    Create Screen2View.hpp

---
+++ Screen2View.hpp
@@ -0,0 +1,27 @@
+#ifndef SCREEN2VIEW_HPP
+#define SCREEN2VIEW_HPP
+
+#include <gui_generated/screen2_screen/Screen2ViewBase.hpp>
+#include <gui/screen2_screen/Screen2Presenter.hpp>
+#include <touchgfx/widgets/Button.hpp>
+extern touchgfx::Button FadeToggleB;
+
+
+class Screen2View : public Screen2ViewBase
+{
+public:
+    Screen2View();
+    virtual ~Screen2View() {}
+    virtual void setupScreen();
+    virtual void tearDownScreen();
+    virtual void FadeF();
+    virtual void FadeTogglePressed();
+    virtual void buttonClicked(const touchgfx::AbstractButton& source);
+    bool getFadeToggleState() const; // Ensure this is declared
+
+protected:
+    touchgfx::ToggleButton FadeToggleB;  // Declare the toggle button
+    Callback<Screen2View, const AbstractButton&> buttonClickedCallback;
+};
+
+#endif // SCREEN2VIEW_HPP
